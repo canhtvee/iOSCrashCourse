6398bf03c99a3111614ebcdf3c348ce4f39504e6
TrinhVanCanh <canhtv@rabiloo.com>
2022-06-12 11:27:23 +0700

    Initial Commit

---
+++ iOSCrashCourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSCrashCourse
+//
+//  Created by canh on 12/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSCrashCourse/iOSCrashCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCrashCourseApp.swift
+//  iOSCrashCourse
+//
+//  Created by canh on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSCrashCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
